095f6eabdab6ef4a4cc6e29c341999c71391577d
Daniel Sanchez <dan.sanchez823@gmail.com>
2021-10-15 14:12:12 -0700

    chore(components): add components folder with uppercase files

---
+++ components/folder/NewFolderPage.js
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+import React from 'react';
+
+function NewFolderPage() {
+    return (
+        <View>
+            
+        </View>
+    )
+}
+
+export default NewFolderPage
