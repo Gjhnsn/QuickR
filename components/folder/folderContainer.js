63a305be616a87deb8034c127e4682422f56e64c
Daniel Sanchez <dan.sanchez823@gmail.com>
2021-10-15 11:44:51 -0700

    refactor(components): rename component files, perform core imports and exports

---
+++ components/folder/folderContainer.js
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+import React from 'react';
+
+function FolderContainer() {
+    return (
+        <View>
+            
+        </View>
+    )
+}
+
+export default FolderContainer
---
+++ components/folder/folderContent.js
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+import React from 'react';
+
+function FolderContent() {
+    return (
+        <View>
+            
+        </View>
+    )
+}
+
+export default FolderContent
