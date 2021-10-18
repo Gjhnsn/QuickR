f9661abc6456a871508cfa387f5580910cdc0610
Daniel Sanchez <dan.sanchez823@gmail.com>
2021-10-18 15:51:41 -0700

    feat(qr-container): enable camera access, show camera screen, basic button and camera styling

--- components/QrContainer.js
+++ components/QrContainer.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, Image, Pressable } from 'react-native';
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, Image, Pressable, Alert, Button, Dimensions } from 'react-native';
 import { createQr } from '../utils/createQrApi';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+import { Camera } from 'expo-camera';
 
 const DATA = [
     {
@@ -29,6 +30,18 @@ const DATA = [
   
   const QrContainer = () => {
     const [currentQr, setCurrentQr] = useState(`test`);
+    const [startCamera, setStartCamera] = useState(false);
+    const [cameraType, setCameraType] = useState(Camera.Constants.Type.back)
+
+    const __startCamera = async () => {
+      const {status} = await Camera.requestPermissionsAsync()
+      console.log(status)
+      if (status === 'granted') {
+        setStartCamera(true)
+      } else {
+        Alert.alert('Access denied')
+      }
+    }
 
     const Item = ({ title, url }) => (
       <Pressable onPress={() => setCurrentQr(createQr(url))}>
@@ -49,16 +62,44 @@ const DATA = [
     );
   
     return (
+
       <SafeAreaView style={styles.container}>
-          <Image 
+          {startCamera ? (
+            <View
+            style={{
+              flex: 1,
+              width: Dimensions.get('window').width,
+            }}>
+              <Camera
+                type={cameraType}
+                style={{flex: 1}}
+              >
+              </Camera>
+            </View>
+            ) : (
+              // home screen view
+            <View style={styles.contentContainer}>
+              <Image 
                 style={{width: 300, height: 300}} 
                 source={{uri:currentQr}}
             />
-        <FlatList
-          data={DATA}
-          renderItem={renderItem}
-          keyExtractor={item => item.id}
-        />
+            <Pressable 
+            onPress={__startCamera}
+            style={styles.button}>
+              <Text style={styles.buttonText}>Scan</Text>
+            </Pressable>
+            <FlatList
+              data={DATA}
+              renderItem={renderItem}
+              keyExtractor={item => item.id}
+            />
+            </View>
+            )}
+
+
+
+        <StatusBar style="auto" />
+        
       </SafeAreaView>
     );
   }
@@ -67,6 +108,14 @@ const DATA = [
     container: {
       flex: 1,
       marginTop: StatusBar.currentHeight || 0,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#fff',
+    },
+    contentContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
     },
     item: {
       backgroundColor: '#f9c2ff',
@@ -77,6 +126,20 @@ const DATA = [
     title: {
       fontSize: 32,
     },
+    button: {
+      width: 130,
+        borderRadius: 4,
+        backgroundColor: '#14274e',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 40
+    },
+    buttonText: {
+      color: '#fff',
+      fontWeight: 'bold',
+      textAlign: 'center',
+    }
   });
   
   export default QrContainer;
