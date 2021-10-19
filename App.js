f6bc136ce303e045f064ee8bc81d408e4a0d4b9a
Daniel Sanchez <dan.sanchez823@gmail.com>
2021-10-19 13:11:25 -0700

    refactor(app): self close dashboard component

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <Provider store={store}>
       <View style={styles.container}>
-        <Dashboard></Dashboard>
+        <Dashboard />
       </View>
     </Provider>
   );
