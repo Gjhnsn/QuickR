c05f25e505d4edee284061fdef6838b88f88a220
Daniel Sanchez <dan.sanchez823@gmail.com>
2021-11-15 16:48:58 -0800

    feat(data-persistence): implement redux persist and async storage

--- redux/store.js
+++ redux/store.js
@@ -1,14 +1,31 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import folderSlice from "./folderSlice";
 import startCameraSlice from "./startCameraSlice";
 import qrSlice from "./qrSlice";
 import modalSlice from "./modalSlice";
 
-export default configureStore({
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+};
+
+const persistedFolderState = persistReducer(persistConfig, folderSlice);
+
+const store = configureStore({
   reducer: {
-    folder: folderSlice,
+    folder: persistedFolderState,
     camera: startCameraSlice,
     qr: qrSlice,
     modal: modalSlice,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+const persistor = persistStore(store);
+
+export { persistor, store };
